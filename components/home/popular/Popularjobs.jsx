b063378e6f9508ba14cc771455072bc04add41a9
PoJu <spes9850401@gmail.com>
2023-08-10 12:09:51 +0800

    <fix> using real API call instead of mock data

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRouter } from "expo-router";
 import {
   View,
   Text,
@@ -6,16 +7,21 @@ import {
   FlatList,
   ActivityIndicator,
 } from "react-native";
-import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
-import  PopularJobCard  from "../../common/cards/popular/PopularJobCard";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
+
+  console.log(data);
 
   return (
     <View style={styles.container}>
@@ -34,13 +40,9 @@ const Popularjobs = () => {
         ) : (
           <FlatList
             data={[1, 2, 3, 4, 5]}
-            renderItem={({ item }) => (
-              <PopularJobCard 
-                item={item} 
-              />
-            )}
-            keyExtractor={item => item?.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium}}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
           />
         )}
