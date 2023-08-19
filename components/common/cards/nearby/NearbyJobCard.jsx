8d25dd1ac24d191b5376167b0fef4f8c2e6ee8c4
PoJu <spes9850401@gmail.com>
2023-08-19 10:20:27 +0800

    <feat> job details page

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -9,7 +9,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
         <Image
           source={{
             uri:
-              job.employer_logo ||
+              job.employer_logo ?? 
               "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
           resizeMode="contain"
